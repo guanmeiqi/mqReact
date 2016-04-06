83732c6d64489770db8a8f6e30fab492c329e24a
guanmeiqi <tammy_girl@163.com>
2016-04-06 17:37:54 +0800

    第一个react和webpack项目

---
+++ app/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React, { Component } from 'react';
+//import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './components/Button/Button';
+//ES5
+/*var HelloWorld=React.createClass({
+    render:function(){
+        return (
+            <h1>Hello world</h1>
+        )
+    }
+});*/
+
+//ES6
+/*
+ class HelloWorld extends Component {
+ //class HelloWorld extends React.Component {
+ render(){
+ return (
+ <h1>Hello world！</h1>
+ )
+ }
+ }
+
+ ReactDOM.render(<HelloWorld />, document.getElementById('app'));*/
+let root = document.getElementById('app');
+ReactDOM.render( <Button />, root );
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+
+module.exports = {
+    //热替换
+    /* entry: [
+     'webpack-dev-server/client?http://localhost:8080',
+     path.resolve(__dirname, 'app/index.js')
+     ],*/
+    //react-hot-loader
+    entry: [
+        //'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'app/index.js')
+    ],
+    //最基本的
+    //entry: path.resolve(__dirname, 'app/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    /* module: {
+     loaders: [
+     {
+     test: /\.js$/,
+     loader: 'babel-loader'
+     }
+     ]
+     },*/
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                //loaders: ['react-hot', 'babel'],
+               // exclude: path.resolve(__dirname, 'node_modules')
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.less/,
+                loader: 'style!css!less'
+            }
+        ]
+    }
+   /* plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+        ]*/
+    };
